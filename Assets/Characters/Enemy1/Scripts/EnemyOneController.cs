3da25d7279af22c45a722fe4523f0107523f4ef2
Megu Hisanaga <lemonsquash419@gmail.com>
2025-12-10 01:51:30 +0900

    feat: EnemyOneControllerにPlayer追尾のコードを追加
    ・Playerタグを持つオブジェクトに一定範囲近付いたらその方向に動く

--- Assets/Characters/Enemy1/Scripts/EnemyOneController.cs
+++ Assets/Characters/Enemy1/Scripts/EnemyOneController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyOneController : MonoBehaviour
 {
+    [SerializeField]float serchRadius = 5.0f;
+    [SerializeField]float moveSpeed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class EnemyOneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject player = GameObject.FindWithTag("Player");
+        Vector2 thisPos = this.gameObject.transform.position;
+        Vector2 playerPos = player.transform.position;
+        float distance = Vector2.Distance(thisPos,playerPos);
+
+        if(distance < serchRadius){
+            this.transform.position = Vector2.MoveTowards(this.transform.position, playerPos, moveSpeed*Time.deltaTime);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D){
+        if(collision2D.gameObject.tag == "Player"){
+            Destroy(this.gameObject);
+        }
     }
 }
