d637bd5bb073311b74bcd4ef532156d0acb9f777
Megu Hisanaga <lemonsquash419@gmail.com>
2026-02-18 06:10:08 +0900

    feat: 選択中のボタンがわかるようにUIを追加
    ・四角のSpriteを選択中のボタンの下に配置
    ・新しくボタンクリックで選択したら更新

--- Assets/Others/StageSelectScene_Assets/StageSelecUIController.cs
+++ Assets/Others/StageSelectScene_Assets/StageSelecUIController.cs
@@ -12,18 +12,29 @@ public class StageSelecUIController : MonoBehaviour
     public Button decideButton;
     public StageSelectCameraController cameraController;
     int selectStageNum;
+    public int currentStageNum;
     public string[] sceneName;
+    public GameObject selectRect;
     // Start is called before the first frame update
     void Start()
     {
+        //ステージのボタンの初期化
         int i = 0;
         foreach (Button button in stageButtons)
         {
             int num = i;
-            button.onClick.AddListener(()=>SetStageNum(num));
+            button.onClick.AddListener(()=>SetStageNum(num));//メソッドの割り当て(引数はインデックスと一緒)
+            if(num >= currentStageNum)
+            {
+                button.interactable = false;//現在のステージより後ならボタンを無効化
+            }
             i++;
         }
+
+        //決定ボタンにメソッド割り当て
         decideButton.onClick.AddListener(DecideStage);
+        
+        selectRect.transform.position = stageButtons[selectStageNum].transform.position;//選択UIの初期位置移動
     }
 
     // Update is called once per frame
@@ -35,7 +46,8 @@ public class StageSelecUIController : MonoBehaviour
     public void SetStageNum(int stageNum)
     {
         selectStageNum = stageNum;
-        cameraController.MoveStagePos(stageNum);
+        cameraController.MoveStagePos(stageNum);//カメラの移動
+        selectRect.transform.position = stageButtons[stageNum].transform.position;//選択UIの移動
         Debug.Log($"ステージ:{stageNum}を選択");
     }
 
